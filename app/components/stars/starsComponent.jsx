89c83e330acfa0ee43a04c16eb02f352effb25a8
Nestor Lopez <nestor.lopez.delgadillo@gmail.com>
2017-09-06 23:01:23 -0500

    Add functionality

--- app/components/stars/starsComponent.jsx
+++ app/components/stars/starsComponent.jsx
@@ -4,11 +4,9 @@ import _ from 'lodash';
 import './starsComponent.scss';
 
 const Stars = (props) => {
-    const numberOfStars = _.random(1,9);
-
     return (
         <div className="stars">
-            {_.range(numberOfStars).map(i => 
+            {_.range(props.numberOfStars).map(i =>
                 <i key={i} className="fa fa-star stars__star"></i>
             )}
         </div>
