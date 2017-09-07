50f964d79bc4a381438d30873f51e018a29826cd
Nestor Lopez <nestor.lopez.delgadillo@gmail.com>
2017-09-06 23:00:58 -0500

    Add functionality Add styles

--- app/components/numbers/numbersComponent.jsx
+++ app/components/numbers/numbersComponent.jsx
@@ -3,12 +3,20 @@ import _ from 'lodash';
 
 import './numbersComponent.scss';
 
-const Numbers = () => {
+const Numbers = (props) => {
     // const arrayOfNumbers = _.range(1,10);
+
+    const numberClassName = (number) => {
+        if (props.selectedNumbers.indexOf(number) >= 0)
+            return 'numbers__number selected';
+
+        return 'numbers__number'
+    }
+
     return (
         <div className="numbers">
             {Numbers.list.map((number, i) =>
-                <span key={i} className="numbers__number" >{number}</span>
+                <span key={i} onClick={ () => props.selectNumber(number)} className={numberClassName(number)} >{number}</span>
             )}
         </div>
     );
